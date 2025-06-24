2fdcd75bdc244903941b5d805566355dc07bd176
RossAscends <124905043+RossAscends@users.noreply.github.com>
2025-06-25 01:20:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+# Text Completion Reasoning Profile Extension
+
+## How to Install
+
+Use this URL in SillyTavern's extension installer:
+
+```plaintext
+https://github.com/RossAscends/ST-TCReasoningProfile/
+```
+
+## Concept
+
+This extension draws inspiration from [Stepped Thinking](https://github.com/cierru/st-stepped-thinking) by Cierru, but is written to integrate closely with SillyTavern's native Reasoning Auto-Parse functionality while also utlizing Connection Profiles for more customization.
+
+### Advantages over Stepping Thinking
+
+- Reasoning and Response are contained within the same message and follow ST's native message saving conventions.
+- Following from above, all native functions for Reasoning manipulation are also possible.
+- No new UI to learn for prompt adjustments; just setup Connection Profiles as you normally would.
+- Can use different APIs for each step, leveraging their individual strengths.
+
+### Assumptions
+
+- Not all models are trained to Reason, but they *do* all have an innate 'Assistant Lizard Brain' we can leverage to simulate reasoning.
+- Models that *are* trained to Reasoning tend to spend a lot of time & tokens in their Reasoning process, and we want to avoid this.
+- Models follow complex instructions much more precisely when Temperature is low (0.5 to 0.8).
+- Models write more interesting and novel roleplay content when Temperature is high (1.0 to 1.5 or even more, when mitigated with minP 0.01 to 0.03).
+- The primary use case is local inference with a 12 to 24B model on a Text Completion API like Tabby, YALS, or KCPP.
+
+### Intended Strategy
+
+- Split the generation into two independent steps: Reasoning and Response.
+- Each step functions under its own bespoke Connection Profile.
+- `Reasoning Step` has low temp, a response length of your liking, and should not produce any response after the Reasoning is finished.
+  - **EXCEPTION**: Asking the model produce a minimal 'finished signal' (ex: `CoT Done`) outside the Reasoning tags can be a useful failsafe. This is explained more below.
+- `Response Step` is higher temp, clears any 'Response' content leftover from the Reasoning step, and uses the Reasoning content to inform itself.
+- When a generation request is sent the extension swaps to the Reasoning profile first, gets the Reasoning, and then swaps back to the Response profile to complete the request.
+
+### Limitations
+
+- Since you are effectively making two separate API requests, it may take longer if your backend is slow with Prompt Processing.
+  - In my testing (RTX3060 12GB VRAM running a 24B 2.5bpw EXL3 model in Tabby at 8k Q8 context) I found a full response of 500 reasoning tokens and 1000 response tokens took about 60-80 seconds. However this was pushing my card to the limit. Models that properly fit your GPU will be significantly faster.
+- You have to setup the Connection Profiles on your own.
+- Will probably break if you try to use it alongside Stepped Thinking or any other Extension/Quick Reply that interferes with ST's native message processing functions.
+- You must remember to save any presets that are a part of your connection profiles if you do any Sampler or Prompt adjustments.
+- You must remember to save the Connection Profile itself if you swap any of the presets associated with it.
+- It probably won't work on Chat Completion APIs, but there are better, usually native, tools for managing Reasoning on those APIs/models already.
+
+## Post-install Setup
+
+### Requirements
+
+- Connection Manager extension
+- `Reasoning Auto Parse` configured in ST's Advanced Formatting panel
+- At least 2 Connection Profiles
+
+### Extension Settings
+
+**Power button** - located in the Extension Settings header, this simply toggles the active state of the entire extension.
+**Reasoning Profile** - this is the profile that will be used in the Reasoning Step.
+**Post-Reasoning auto-Continued Response Prefix** textarea - The message about to be continued upon will be prefixed with this. By default the extension will add `\n ` (newline and a space), but you can set this to anything you like. This prefix helps assure the model will attempt to Continue correctly and not simply assume the response is complete and thus return `<EOS>` or something similar.
+**Continue After Reasoning Finishes** checkbox - makes the extension do the following after Reasoning Step is complete:
+
+- Swap back to the primary Response Profile
+- Clear any extra content outside the `<reasoning>` section
+- Add the header prefix to help assure the model will continue successfully
+- Finally, initiate a Continue to get the Response portion
+
+### Profile Setup
+
+#### Reasoning Connection Profile
+
+This should be designed to get the model to do a chain of thought inside tags that match your Reasoning prefix/suffix settings in ST's Advanced Formatting panel. I have found that models tend to dislike doing this cleanly, so it may be helpful to prompt it to include a single word after the reasoning tag has closed to signify it has are finished. Without such a 'final word outside of the think tag', the model will often forget to close the think tag.
+
+**Sample Reasoning Profile Settings:**
+
+```plaintext
+- Samplers Panel -
+Temp: 0.5
+MinP: 0.01
+Response Length: 500 tokens
+
+- Advanced Formatting Panel -
+Reasoning AutoParse: ENABLED
+Reasoning Prefix: <think>\n
+Reason Suffix: \n</think>
+Prefix reply with: <think>\n
+```
+
+**NOTE:** The `prefix reply with` setting is unnecessary if your model naturally produces Reasoning tags on its own.
+
+**Sample Reasoning Profile System Prompt:**
+
+```plaintext
+Do not roleplay as {{char}} in the next response. 
+Instead, provide a concise out-of-character Chain of Thought (CoT) in the form of a bullet list.
+The CoT should illustrate {{char}}'s thoughts regarding the story so far and {{user}}'s latest input. 
+Format the CoT exactly as shown below without deviation:
+
+---
+<think>
+
+- Concise thoughts in a bullet list.
+- Concise personality traits in a bullet list.
+- Concise background information in a bullet list.
+
+</think>
+---
+
+Your response will be invalid if you do not write </think> at the end.
+After closing the 'think' XML tag, simply write "CoT Done" and end your response IMMEDIATELY.
+Do not respond in-character as {{char}} after the CoT.
+Instructions, background information, and story details follow below:
+```
+
+**NOTE:** The purpose of asking the model to write `CoT Done` outside the `<think>` tag is to assure the model actually closes the tag before ending the token stream.  However, not all models will require this kind of a crutch (but 24B Mistrall-Small-3.2-2506 *did*, because it's not a Reasoning model). The `CoT Done` text will be removed in the next step and replaced with the content of the Extension's `Post-Reasoning auto-Continued Response Prefix` textarea before the next prompt is sent to the model to ask for the Response content.
+
+#### Response Connection Profile
+
+**NOTE:** Whatever Profile is active when you send the API request will be used in the Response Step!
+
+**Sample Response Profile Settings:**
+
+```plaintext
+Temp 1.5
+MinP 0.015
+Response Length 1000 tokens
+```
+
+**Sample Response Profile System Prompt:**
+
+This can be anything, but I find it helps to adjust the system prompt to ask the model to `continue with a response based on the Reasoning contents` or something of that sort.
+
+```plaintext
+Continue in-character as {{char}} to provide a final response that is informed by and integrates the concepts detailed inside the <think> tag. 
+Do not directly mention or quote verbatim the contents inside the <think> tag.
+```
+
+## License
+
+AGPLv3
---
+++ index.js
@@ -0,0 +1,260 @@
+const {
+    eventSource,
+    event_types,
+} = SillyTavern.getContext();
+
+import { saveSettingsDebounced, saveChat, stopGeneration } from '../../../../script.js';
+import { delay } from '../../../utils.js';
+import { extension_settings, getContext } from '../../../extensions.js';
+
+
+import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
+//import { SlashCommandAbortController } from '../../slash-commands/SlashCommandAbortController.js';
+//import { ARGUMENT_TYPE, SlashCommandArgument, SlashCommandNamedArgument } from '../../slash-commands/SlashCommandArgument.js';
+
+const LOG_PREFIX = '[ST-TCReasoningProfile]';
+const EXTENSION_PATH = 'scripts/extensions/third-party/ST-TCReasoningProfile';
+
+const $connectionProfilesSelect = $('#connection_profiles');
+
+let activeConnectionProfileName = null;
+let isReasoningProfileSwappedOn = false;
+let isExtensionActive = false;
+
+let isMidGenerationCycle = false;
+let isAutoContinuing = false;
+let isProfileSwapping = false;
+
+let settings = {};
+
+function initExtSettings() {
+    extension_settings.customReasoning = extension_settings.customReasoning || {};
+    extension_settings.customReasoning.reasoningProfileID = extension_settings.customReasoning.reasoningProfileID || 'None';
+    extension_settings.customReasoning.reasoningProfileName = extension_settings.customReasoning.reasoningProfileName || 'None';
+    extension_settings.customReasoning.autoContinueAfterReasoning = extension_settings.customReasoning.autoContinueAfterReasoning || false;
+    extension_settings.customReasoning.isExtensionActive = extension_settings.customReasoning.isExtensionActive || false;
+    extension_settings.customReasoning.postReasoningPrefix = extension_settings.customReasoning.postReasoningPrefix || '\n ';
+}
+
+function getExtSettings() {
+    return extension_settings.customReasoning;
+}
+
+function addConnectionProfilesToExtension() {
+
+    if (!$('connection_profiles')) return;
+
+    //console.warn('Adding connection profiles to extension selector');
+    const context = getContext();
+    const profiles = context.extensionSettings?.connectionManager?.profiles || [];
+    const $extensionSelector = $('#reasoningProfileSelector');
+    //console.warn(`Found ${profiles.length} connection profiles`);
+    $extensionSelector.append('<option value="None">None</option>');
+    for (const profile of profiles) {
+        $extensionSelector.append(`<option value="${profile.id}">${profile.name}</option>`);
+    }
+    $extensionSelector.val(extension_settings.customReasoning.reasoningProfileID).trigger('change');
+    $extensionSelector.off('change').on('change', () => updateExtensionSettings());
+
+}
+
+async function updateExtensionSettings() {
+    const $extensionSelector = $('#reasoningProfileSelector');
+    const profileName = $extensionSelector.find('option:selected').text();
+    const profileID = $extensionSelector.find('option:selected').val();
+    console.info(` ${LOG_PREFIX} Updating reasoning profile to "${profileName}"`);
+    extension_settings.customReasoning.reasoningProfileID = profileID;
+    extension_settings.customReasoning.reasoningProfileName = profileName;
+    await saveSettingsDebounced();
+}
+
+async function swapToReasoningProfile() {
+
+    if (extension_settings.customReasoning.reasoningProfile == 'None') {
+        console.warn(LOG_PREFIX, 'No reasoning profile selected');
+        isReasoningProfileSwappedOn = false;
+        return;
+    }
+
+    activeConnectionProfileName = $connectionProfilesSelect.find('option:selected').text();
+    console.debug(`${LOG_PREFIX} Saving active main profile as "${activeConnectionProfileName}" for later reversion.`);
+    isProfileSwapping = true;
+    isReasoningProfileSwappedOn = true;
+    console.debug(`${LOG_PREFIX} Swapping to reasoning profile ${extension_settings.customReasoning.reasoningProfileName}`);
+    try {
+        await SlashCommandParser.commands['profile'].callback(
+            {
+                await: 'true',
+                _scope: null, // or a valid SlashCommandScope instance
+                _abortController: null, // or a valid SlashCommandAbortController instance
+            },
+            extension_settings.customReasoning.reasoningProfileName,
+        );
+        await new Promise(resolve => setTimeout(() => {
+            isProfileSwapping = false;
+            resolve();
+        }, 500));
+    } catch (error) {
+        console.error(`Failed to swap to reasoning profile: ${error}`);
+    }
+}
+
+async function swapToOriginalProfile() {
+    console.debug(`${LOG_PREFIX} Swapping back to original profile: "${activeConnectionProfileName}"`);
+    isProfileSwapping = true;
+    try {
+        await SlashCommandParser.commands['profile'].callback(
+            {
+                await: 'true',
+                _scope: null, // or a valid SlashCommandScope instance
+                _abortController: null, // or a valid SlashCommandAbortController instance
+            },
+            activeConnectionProfileName,
+        );
+        await new Promise(resolve => setTimeout(() => {
+            isProfileSwapping = false;
+            isReasoningProfileSwappedOn = false;
+            resolve();
+        }, 500));
+
+    } catch (error) {
+        console.error(`Failed to swap to reasoning profile: ${error}`);
+    }
+}
+
+function toggleExtensionState(state) {
+    const $activeToggle = $('#customReasoningPowerButton');
+    //console.warn(`Toggling extension active state to ${state}`);
+    $activeToggle.toggleClass('toggleEnabled', state);
+    extension_settings.customReasoning.isExtensionActive = state;
+    saveSettingsDebounced();
+}
+
+(async function () {
+
+    //console.warn('Custom Reasoning extension loaded');
+    const settingsHtml = await $.get(`${EXTENSION_PATH}/settings.html`);
+    $('#extensions_settings').append(settingsHtml);
+
+    const $extensionSelector = $('#reasoningProfileSelector');
+    const $activeToggle = $('#customReasoningPowerButton');
+    const $autoContinue = $('#autoContinueAfterReasoning'); //checkbox
+    const $postReasoningPrefix = $('#postReasoningPrefix');
+
+    settings = getExtSettings();
+    let isAnySettingNull = false;
+    let whichSetting = null;
+
+    if (settings) {
+        for (const [key, value] of Object.entries(settings)) {
+            if (value === null) {
+                isAnySettingNull = true;
+                whichSetting = key;
+                break;
+            }
+        }
+    }
+
+    if (!settings || isAnySettingNull) {
+        console.warn(`${LOG_PREFIX} No settings found, or something was Null (${whichSetting}); initializing`);
+        initExtSettings();
+        settings = getExtSettings();
+    }
+
+    console.info(`${LOG_PREFIX} Extension settings ready: ${JSON.stringify(settings)}`);
+
+    eventSource.once(event_types.APP_READY, () => {
+        addConnectionProfilesToExtension();
+        $extensionSelector.val(settings.reasoningProfileID).trigger('change');
+        $autoContinue.prop('checked', settings.autoContinueAfterReasoning);
+        $postReasoningPrefix.val(settings.postReasoningPrefix);
+        isExtensionActive = settings.isExtensionActive;
+        activeConnectionProfileName = $connectionProfilesSelect.find('option:selected').text();
+        console.debug(`${LOG_PREFIX} onLoad active connection profile is: ${activeConnectionProfileName}`);
+        toggleExtensionState(isExtensionActive);
+    });
+
+    $activeToggle.off('click').on('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        isExtensionActive = !isExtensionActive;
+        toggleExtensionState(isExtensionActive);
+    });
+
+    $autoContinue.off('click').on('click', (e) => {
+        extension_settings.customReasoning.autoContinueAfterReasoning = !extension_settings.customReasoning.autoContinueAfterReasoning;
+        saveSettingsDebounced();
+    });
+
+    $postReasoningPrefix.off('change').on('change', (e) => {
+        extension_settings.customReasoning.postReasoningPrefix = $postReasoningPrefix.val();
+        saveSettingsDebounced();
+    });
+
+
+    eventSource.on(event_types.GENERATION_STARTED, async () => {
+        console.debug(`Generation started; isMidGenerationCycle? ${isMidGenerationCycle}, isAutoContinuing? ${isAutoContinuing}`);
+        if (!isExtensionActive) return;
+        isMidGenerationCycle = true;
+
+        if (!isReasoningProfileSwappedOn && isExtensionActive && !isAutoContinuing) {
+            console.debug(LOG_PREFIX, 'Swapping to reasoning Profile');
+            await swapToReasoningProfile();
+            eventSource.once(event_types.STREAM_REASONING_DONE, async () => {
+                console.debug(LOG_PREFIX, 'STREAM_REASONING_DONE, stopping Generation.');
+                stopGeneration();
+            });
+        }
+        if (isExtensionActive && isAutoContinuing) {
+            console.debug(LOG_PREFIX, 'AUTOCONTINUING');
+
+            /*
+            // uncomment to see exactly what was sent
+            eventSource.once(event_types.GENERATE_AFTER_DATA, async (generate_data) => {
+                console.warn(generate_data.prompt);
+            });
+            */
+
+            let chat = getContext().chat;
+            let lastMes = chat[chat.length - 1];
+
+            console.debug(LOG_PREFIX, 'PRIMING CONTINUE WITH PREFIX');
+            lastMes.mes = extension_settings.customReasoning.postReasoningPrefix;
+            chat[chat.length - 1] = lastMes;
+            await saveChat();
+            await delay(200);
+        }
+    });
+
+    eventSource.on(event_types.GENERATION_ENDED, async () => {
+        if (!isExtensionActive) return;
+        console.debug(LOG_PREFIX, 'Generation ended');
+        await delay(200);
+        console.debug(`${LOG_PREFIX} MidGeneration? ${isMidGenerationCycle}, isAutoContinuing? ${isAutoContinuing}`);
+        if (isReasoningProfileSwappedOn && isExtensionActive && !isProfileSwapping) {
+            console.debug(LOG_PREFIX, 'G_ENDED; reverting');
+            await swapToOriginalProfile();
+        }
+        if (!extension_settings.customReasoning.autoContinueAfterReasoning && isMidGenerationCycle) {
+            isMidGenerationCycle = false;
+        }
+        if (isAutoContinuing && isMidGenerationCycle) {
+            console.debug(LOG_PREFIX, 'clearing auto-continue and midcycle tags since we should be done.');
+            isAutoContinuing = false;
+            isMidGenerationCycle = false;
+        }
+        if (extension_settings.customReasoning.autoContinueAfterReasoning && isMidGenerationCycle && !isAutoContinuing) {
+            console.debug(LOG_PREFIX, 'triggering auto-continue since we are still midcycle and havent done the continued response part yet');
+            isAutoContinuing = true;
+            $('#option_continue').trigger('click'); //old school smoothbrained method still used in script.js!
+        }
+        console.debug(`${LOG_PREFIX} AFTER GEND: MidGeneration? ${isMidGenerationCycle}, isAutoContinuing? ${isAutoContinuing}`);
+    });
+
+    eventSource.on(event_types.CONNECTION_PROFILE_LOADED, () => {
+        if (!isExtensionActive) return;
+        if (isReasoningProfileSwappedOn || isMidGenerationCycle || isAutoContinuing) { return; } //so we don't trigger on our own change
+        console.debug(`${LOG_PREFIX} Main connection profile changed to ${activeConnectionProfileName}`);
+        activeConnectionProfileName = $connectionProfilesSelect.find('option:selected').text();
+    });
+})();
